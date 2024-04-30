93ecff3efd671f9da632b3a664c0747381cd0878
Patrick Gross <bears_lair@protonmail.com>
2024-04-29 20:30:38 -0700

    final solution

--- romanNumeralToDigit.js
+++ romanNumeralToDigit.js
@@ -1,44 +1,46 @@
-// decimal variable set to 0
-// string to array
-// iterate through array, adding RN value to integer
-// IF Roman
-
 function solution(roman) {
   let decimal = 0;
-  const decimalArray = roman.split("");
-  decimalArray.map((letter) => values(letter));
+  let decimalArray = [];
+  const romanArray = roman.split("");
+
+  romanArray.map((letter) => {
+    switch (letter) {
+      case "I":
+        decimalArray.push(1);
+        break;
+      case "V":
+        decimalArray.push(5);
+        break;
+      case "X":
+        decimalArray.push(10);
+        break;
+      case "L":
+        decimalArray.push(50);
+        break;
+      case "C":
+        decimalArray.push(100);
+        break;
+      case "D":
+        decimalArray.push(500);
+        break;
+      case "M":
+        decimalArray.push(1000);
+        break;
+    }
+  });
+
+  for (let i = 0; i < decimalArray.length; i++) {
+    if (decimalArray.length === 1) {
+      decimal += decimalArray[i];
+      break;
+    } else if (decimalArray.length > 1 && i === 0) {
+      decimal += decimalArray[0];
+    } else if (decimalArray[i - 1] < decimalArray[i]) {
+      decimal += decimalArray[i] - decimalArray[i - 1] * 2;
+    } else {
+      decimal += decimalArray[i];
+    }
+  }
+
   return decimal;
 }
-
-function values(letter) {
-  switch (letter) {
-    case "I":
-      decimal += 1;
-      break;
-    case "V":
-      decimal += 5;
-      break;
-    case "X":
-      decimal += 10;
-      break;
-    case "L":
-      decimal += 50;
-      break;
-    case "C":
-      decimal += 100;
-      break;
-    case "D":
-      decimal += 500;
-      break;
-    case "M":
-      decimal += 1000;
-      break;
-  }
-}
-
-//Test cases
-console.log(solution("XXI")); // 21
-console.log(solution("I")); // 1
-console.log(solution("IV")); // 4
-console.log(solution("MMVIII")); // 2008
-console.log(solution("MDCLXVI")); // 1666
