0377782fb6ee0f3f4845ad0e7b6fbb55842d5c21
Patrick Gross <bears_lair@protonmail.com>
2024-04-29 17:42:01 -0700

    switch statement created

--- romanNumeralToDigit.js
+++ romanNumeralToDigit.js
@@ -4,9 +4,36 @@
 // IF Roman
 
 function solution(roman) {
-  // let decimal = 0;
+  let decimal = 0;
   const decimalArray = roman.split("");
-  return decimalArray;
+  decimalArray.map((letter) => values(letter));
+  return decimal;
+}
+
+function values(letter) {
+  switch (letter) {
+    case "I":
+      decimal += 1;
+      break;
+    case "V":
+      decimal += 5;
+      break;
+    case "X":
+      decimal += 10;
+      break;
+    case "L":
+      decimal += 50;
+      break;
+    case "C":
+      decimal += 100;
+      break;
+    case "D":
+      decimal += 500;
+      break;
+    case "M":
+      decimal += 1000;
+      break;
+  }
 }
 
 //Test cases
